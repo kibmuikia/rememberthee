bc513c404444036341ec36ebd0462a45ca50953c
kibmuikia <muikiaa@gmail.com>
2019-09-06 18:29:56 +0300

    registration backend v1

--- lib/views/auth/register.dart
+++ lib/views/auth/register.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'user.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -6,10 +8,66 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
-  final _formKey = GlobalKey<FormState>();
+  // final _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  // TextEditingController controllerEmail = new TextEditingController();
+  User newUser = new User();
+
+  void processSignUp() async {
+    // var url = "http://127.0.0.1/remthee/process_signup.php";
+    var url = "https://rememberthee.com/android/process_signup.php";
+
+    final FormState form = _formKey.currentState;
+
+    if (!form.validate()) {
+      showMessage('Form is not valid!  Please review and correct.');
+    } else {
+      form.save(); //This invokes each onSaved event
+      print('\tForm save called, newUser is now up to date...');
+
+      if (newUser.password != newUser.passwordconfirm) {
+        showMessage("Ensure BOTH passwords are similar !");
+        // return false;
+      } else {
+        print( "\tAbout to send data :: " );
+        print( '\tEmail: ${newUser.email}');
+        print( '\tFirst Name: ${newUser.fname}');
+
+        try
+        {
+          final response = await http.post(url, body: {
+            "regFname": newUser.fname,
+            "regLname": newUser.lname,
+            "regEmail": newUser.email,
+            "regPhone": newUser.phone,
+            "regPwd": newUser.password
+          });
+          // print(response);
+          print(response.statusCode);
+          // print(response.headers);
+          // print(response.reasonPhrase);
+          print(response.body);
+        } catch(e) {
+          print("Server Error !!!");
+          print(e);
+          throw Exception('Sign-Up Error :: '+ e );
+        }
+      }//end-else-pwd-check
+    }//end-else-form-valid
+
+  }//end-class-processSignUp
+
+  void showMessage(String message, [MaterialColor color = Colors.red]) {
+    _scaffoldKey.currentState.showSnackBar(
+        new SnackBar(backgroundColor: color, content: new Text(message)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: new AppBar(
         title: new Text('Sign Up'),
         backgroundColor: Colors.deepOrangeAccent,
@@ -20,6 +78,35 @@ class _RegisterPageState extends State<RegisterPage> {
               child: Column(
                 children: <Widget>[
                   TextFormField(
+                    // controller: controllerEmail,
+                    decoration: InputDecoration(
+                      labelText: "Enter your First Name",
+                      hoverColor: Colors.lightBlueAccent,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter First Name';
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => newUser.fname = val,
+                  ),
+                  TextFormField(
+                    // controller: controllerEmail,
+                    decoration: InputDecoration(
+                      labelText: "Enter your Last Name",
+                      hoverColor: Colors.lightBlueAccent,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter Last Name';
+                      }
+                      return null;
+                    },
+                    onSaved: (val) => newUser.lname = val,
+                  ),
+                  TextFormField(
+                    // controller: controllerEmail,
                     decoration: InputDecoration(
                       labelText: "Enter your email",
                       hoverColor: Colors.lightBlueAccent,
@@ -30,18 +117,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       }
                       return null;
                     },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Enter your username",
-                      hoverColor: Colors.lightBlueAccent,
-                    ),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Please enter username';
-                      }
-                      return null;
-                    },
+                    onSaved: (val) => newUser.email = val,
                   ),
                   TextFormField(
                     decoration: InputDecoration(
@@ -54,6 +130,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       }
                       return null;
                     },
+                    onSaved: (val) => newUser.phone = val,
                   ),
                   TextFormField(
                     decoration: InputDecoration(
@@ -66,6 +143,7 @@ class _RegisterPageState extends State<RegisterPage> {
                       }
                       return null;
                     },
+                    onSaved: (val) => newUser.password = val,
                   ),
                   TextFormField(
                     decoration: InputDecoration(
@@ -78,19 +156,21 @@ class _RegisterPageState extends State<RegisterPage> {
                       }
                       return null;
                     },
+                    onSaved: (val) => newUser.passwordconfirm = val,
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 16.0),
                     child: RaisedButton(
-                      onPressed: () {
-                        // Validate returns true if the form is valid, or false
-                        // otherwise.
-                        if (_formKey.currentState.validate()) {
-                          // If the form is valid, display a Snackbar.
-                          Scaffold.of(context).showSnackBar(
-                              SnackBar(content: Text('Processing Data')));
-                        }
-                      },
+                      // onPressed: () {
+                      //   // Validate returns true if the form is valid, or false
+                      //   // otherwise.
+                      //   if (_formKey.currentState.validate()) {
+                      //     // If the form is valid, display a Snackbar.
+                      //     Scaffold.of(context).showSnackBar(
+                      //         SnackBar(content: Text('Processing Data')));
+                      //   }
+                      // },
+                      onPressed: processSignUp,
                       child: Text('Register'),
                     ),
                   ),
